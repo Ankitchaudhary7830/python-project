1f306a4a9bc225c759e504c2544c91bbbf6adb5d
Ankitchaudhary7830 <ankitlohkna@gmail.com>
2023-02-02 13:51:55 +0530

    Feature 1 working successfully

---
+++ ModulesExample.py
@@ -0,0 +1,3 @@
+import firstproject
+print("This is example of importing module")
+firstproject.add(56,12)
---
+++ firstproject.py
@@ -0,0 +1,4 @@
+#add of two numbers using function
+def add(a,b):
+    print("sum of a and b is",(a+b))
+add(4,5)    
